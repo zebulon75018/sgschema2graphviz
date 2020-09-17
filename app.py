bbc9040b982a00dd576b69b22a7bd6111b5e6504
zebulon75018 <zebulon75018@yahoo.fr>
2020-09-18 00:24:08 +0200

    Add the getschema route

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template, url_for
+
 import json
+import os
+import subprocess
 
 app = Flask(__name__)
+#
+# Route.
+#
+@app.route('/getschema',methods=["GET", "POST"])
+def getschema():
 
+    sd = request.form.get("subdomain","")
+    sn = request.form.get("scriptname","")
+    sk = request.form.get("scriptkey","")
+    subprocess.run(["python",
+                "getshemaSG.py",
+                "--nameserver",
+                sd,
+                "--scriptname",
+                sn,
+                "--scriptkey",
+                sk,
+                ])
+    return ""
 
+# faire generation de class python ...
 @app.route('/')
 def home():
+    if os.path.exists('schema.json') is False:
+        return render_template('shotgun.html')
+
     with open('schema.json') as f:
         data = json.load(f)
     return render_template('index.html', entities=data)
